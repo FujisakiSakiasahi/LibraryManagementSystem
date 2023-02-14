9997deb673ed6b373fda3a53438fd155962c4ca7
Viwyn <riaruplays@gmail.com>
2023-02-14 10:34:16 +0800

    updated the sql file

--- data.sql
+++ data.sql
@@ -4,31 +4,46 @@ create table Book (
     bookName VARCHAR(50) NOT NULL,
     authorName VARCHAR(50) NOT NULL,
     bookDescription VARCHAR(150),
-    publisherName VARCHAR(50),
+    publisherName VARCHAR(50) NOT NULL,
+    publishYear INT NOT NULL,
     rating INT
         CHECK (rating BETWEEN 1 AND 5),
     pubDate DATE NOT NULL,
     lan VARCHAR(10) NOT NULL,
-    isbn INT(13)
+    isbn INT(13),
 
     PRIMARY KEY (bookId)
 )
 
+create table Member (
+    memberId INT NOT NULL,
+    memberName VARCHAR(50) NOT NULL,
+    memberNo INT NOT NULL,
+    memberEmail VARCHAR (30) NOT NULL
+        CHECK (memberEmail LIKE '%_@__%.__%'),
+    newsletter BOOLEAN NOT NULL,
+
+    PRIMARY KEY (memberId)
+)
+
 create table Borrowed (
     bookId INT NOT NULL,
-    studentId INT NOT NULL,
+    memberId INT NOT NULL,
     dateBorrowed DATE NOT NULL,
+    returned BOOLEAN NOT NULL,
 
-    PRIMARY KEY (bookId,studentId),
-    FOREIGN KEY (bookId) REFERENCES Book (BookId)
+    PRIMARY KEY (bookId,memberId),
+    FOREIGN KEY (bookId) REFERENCES Book (BookId),
+    FOREIGN KEY (memberId) REFERENCES Member (memberId)
 )
 
 create table Ratings (
     bookId INT NOT NULL,
-    studentId INT NOT NULL,
+    memberId INT NOT NULL,
     rating INT NOT NULL
         CHECK (rating BETWEEN 1 AND 5),
 
-    PRIMARY KEY (bookId,studentId)
-    FOREIGN KEY (bookId) REFERENCES Book (BookId)
+    PRIMARY KEY (bookId,memberId),
+    FOREIGN KEY (bookId) REFERENCES Book (BookId),
+    FOREIGN KEY (memberId) REFERENCS Member (memberId)
 )
