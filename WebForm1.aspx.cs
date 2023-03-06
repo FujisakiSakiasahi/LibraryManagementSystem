f482714ee8d66f3f720809f21fbe384caf47b1a9
FujisakiSakiasahi <90231449+FujisakiSakiasahi@users.noreply.github.com>
2023-03-06 20:57:28 +0800

    runQuery function for the use of getting data from database

--- WebForm1.aspx.cs
+++ WebForm1.aspx.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -9,9 +11,26 @@ namespace LibraryManagementSystem
 {
     public partial class WebForm1 : System.Web.UI.Page
     {
+        private SqlConnection connection = new SqlConnection("Server=localhost\SQLEXPRESS ; Database=SchoolDb ; Integrated Security = TRUE");
+
         protected void Page_Load(object sender, EventArgs e)
         {
 
+        }
+
+        //for the use of getting data from database by running SQL query
+        protected DataTable runQuery(String query) 
+        {
+            DataTable returningData = new DataTable();
+
+            connection.Open();
+
+            SqlDataAdapter da = new SqlDataAdapter(new SqlCommand(query, connection));
+            da.Fill(returningData);
+
+            connection.Close();
+
+            return returningData;
         }
     }
 }
