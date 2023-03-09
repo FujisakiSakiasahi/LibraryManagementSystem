3107e7275e37f0af8e45b3d3c136e0f296ba4ff0
FujisakiSakiasahi <90231449+FujisakiSakiasahi@users.noreply.github.com>
2023-03-09 16:27:06 +0800

    search function and book recommendation funciton

--- WebForm1.aspx.cs
+++ WebForm1.aspx.cs
@@ -11,11 +11,14 @@ namespace LibraryManagementSystem
 {
     public partial class WebForm1 : System.Web.UI.Page
     {
-        private SqlConnection connection = new SqlConnection("Server=localhost\SQLEXPRESS ; Database=SchoolDb ; Integrated Security = TRUE");
+        private SqlConnection connection = new SqlConnection("Server=localhost\\SQLEXPRESS ; Database=LibrarylDb ; Integrated Security = TRUE");
+        //private SqlConnection connection = new SqlConnection("Server=localhost\\SQLEXPRESS ; Database=LibrarylDb ; Integrated Security = TRUE");
+
+        public bool userLoginState = false;
 
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            
         }
 
         //for the use of getting data from database by running SQL query
@@ -31,6 +34,52 @@ namespace LibraryManagementSystem
             connection.Close();
 
             return returningData;
+        }
+
+        protected void Search(String searchStatement, String filter) 
+        {
+            String searchQuery = "SELECT * FROM Book WHERE " + filter + "=" + searchStatement + ";";
+            Datatable result = runQuery(searchQuery);
+
+
+            //set the returning datatable to the required space
+            //write here
+        }
+
+        ///!!!! to-be written after the advanced search UI is done
+        protected void Search(String title, String author, String lang, int isbn, bool availablility)
+        {
+            String searchQuery = "SELECT * FROM Book WHERE ";
+            if (!String.IsNullOrEmpty(title)) {
+                searchQuery += "bookName='" + title + "'";
+            }
+        }
+
+        protected void SetBookRecommendation(String genre){
+            //get a list of book from the database
+            String query = new String();
+            if(genre == "random"){//random genre
+                query = "SELECT DISTINCT genre FROM Genre;"
+                DataTable genreListData = runQuery(query);
+                int genreCount = genreListData.Rows.Count;
+                
+                String[] genreList = new String[genreCount];
+                for(int i = 0 ; i < genreCount ; i++){
+                    genreList[i] = genreListData.Rows[i].Item;
+                }
+
+                //randomly select one genre
+                Random rnd = new Random();
+                int genreIndexChosen = rnd.Next(genreCount);
+
+                //set query
+                String query = "SELECT bookName, authorName, bookImage FROM Book WHERE bookId IN (SELECT bookId FROM genre WHERE genre='"+genreList[genreIndexChosen]+"';);";
+            }else{//specific genre
+                String query = "SELECT bookName, authorName, bookImage FROM Book WHERE bookId IN(SELECT bookId FROM genre WHERE genre='"+genre+"';)";
+            }
+            Datatable result = runQuery(query);
+
+            //set the books to the page here
         }
     }
 }
