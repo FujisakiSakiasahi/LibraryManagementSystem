fd87c4d29355eebc56f10b552a3b542a78216fe2
Viwyn <riaruplays@gmail.com>
2023-03-29 15:18:55 +0800

    why is VS so dumb i hate my life

--- AboutUs.aspx.cs
+++ AboutUs.aspx.cs
@@ -7,7 +7,7 @@ using System.Web.UI.WebControls;
 
 namespace LibraryManagementSystem
 {
-    public partial class WebForm2 : System.Web.UI.Page
+    public partial class WebForm4 : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
         {
