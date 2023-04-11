51d862c522ab37dfa80653ef39f46b762ba34d7a
Jhun Kit <jhunkit2753@gmail.com>
2023-04-11 17:49:33 +0800

    request page ready for function

--- Request.aspx.designer.cs
+++ Request.aspx.designer.cs
@@ -67,5 +67,23 @@ namespace LibraryManagementSystem
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.HtmlControls.HtmlAnchor Logout_Button;
+
+        /// <summary>
+        /// Textbox_Request control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox Textbox_Request;
+
+        /// <summary>
+        /// Button_Request control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button Button_Request;
     }
 }
