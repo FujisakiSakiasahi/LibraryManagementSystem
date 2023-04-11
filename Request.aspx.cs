28ad17bb0647f0e6f821f5e182b127be7447002e
Jhun Kit <jhunkit2753@gmail.com>
2023-04-11 10:13:21 +0800

    Setting, Notifiaction, Request, and FAQ have header fixed

--- Request.aspx.cs
+++ Request.aspx.cs
@@ -9,9 +9,51 @@ namespace LibraryManagementSystem
 {
     public partial class Request : System.Web.UI.Page
     {
+        private SessionHandler sessionHandler = new SessionHandler();
         protected void Page_Load(object sender, EventArgs e)
         {
+            SetInitialLoginState();
+            HeaderUIHandler();
+        }
 
+        protected void HeaderUIHandler()
+        {
+            login_link.Visible = false;
+            profile.Visible = false;
+
+            librarian_link.Visible = false;
+
+            if (sessionHandler.GetLoginState() == false)
+            {
+                login_link.Visible = true;
+            }
+            else
+            {
+                profile.Visible = true;
+            }
+
+            if (sessionHandler.GetIsLibrarian())
+            {
+                librarian_link.Visible = true;
+            }
+        }
+
+        protected void SetInitialLoginState()
+        {
+            if (Session["loginState"] != null)
+            {
+                sessionHandler.CheckLoginState();
+            }
+            else { Session["loginState"] = "false"; }
+        }
+
+        protected void Logout_Function(object sender, EventArgs e)
+        {
+            string link = sessionHandler.GetIsLibrarian() && Request.RawUrl.Equals("Librarian.aspx") ? "Home.aspx" : Request.RawUrl;
+
+            Session["userLoginState"] = false;
+            Session.Abandon();
+            Response.Redirect(link);
         }
     }
 }
